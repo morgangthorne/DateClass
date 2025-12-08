203310f7b04750abe70c1495f3d0f42b4b4ba4bd
morgan thorne <morgangthorne@outlook.com>
2025-12-08 16:52:10 -0500

    Made a "skeleton" in Date.h

--- Date.h
+++ Date.h
@@ -1,1 +1,31 @@
-#pragma once
+#ifndef DATE_H
+#define Date_H
+
+#include <strings>
+
+class Date {
+public:
+
+	//Constructor proto
+
+	//Mutator
+
+	//Accessors
+
+	//Leap year overloads
+
+	//Last-Day overloads
+
+	//print
+
+private:
+
+	int month;
+	int day;
+	int year;
+
+	//Help
+
+};
+
+#endif
